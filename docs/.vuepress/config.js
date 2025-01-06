44283e29020126c10f1fcdaf8a4d6b56293ede6c
kangkang shang <535634238@qq.com>
2025-01-06 10:37:31 +0800

    init cli

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,34 +3,16 @@ import { defineUserConfig } from 'vuepress/cli'
 import { webpackBundler } from '@vuepress/bundler-webpack'
 
 export default defineUserConfig({
-  lang: 'zh-CN', // 默认语言 
+  lang: 'en-US',
+
   title: 'VuePress',
   description: 'My first VuePress Site',
-  locales: {
-    // 键名是该语言所属的子路径
-    // 作为特例，默认语言可以使用 '/' 作为其路径。
-    '/': {
-      lang: 'en-US',
-      title: 'VuePress',
-      description: 'Vue-powered Static Site Generator',
-    },
-    '/zh/': {
-      lang: 'zh-CN',
-      title: 'VuePress',
-      description: 'Vue 驱动的静态网站生成器',
-    },
-  },
+
   theme: defaultTheme({
     logo: 'https://vuejs.press/images/hero.png',
+
     navbar: ['/', '/get-started'],
-    locales: {
-      '/en/': {
-        selectLanguageName: 'English',
-      },
-      '/zh/': {
-        selectLanguageName: '简体中文',
-      },
-    },
   }),
+
   bundler: webpackBundler(),
 })
